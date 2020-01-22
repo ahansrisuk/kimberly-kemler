431c70af14e81ae2a07b750a9ab9c794d5b1ed83
ahansrisuk <ahansrisuk@gmail.com>
2020-01-22 12:19:42 -0500

    initial commit, added tailwindcss

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./tailwind.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,19 +25,12 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
+      <div className="w-full">
+        <main>
+          { children }
+        </main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()}, Designed by Allen Hansrisuk
         </footer>
       </div>
     </>
