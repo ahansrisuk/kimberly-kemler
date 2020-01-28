74dc782491fa9d0cc1872acf7c05521a433fb65f
ahansrisuk <ahansrisuk@gmail.com>
2020-01-27 19:53:12 -0500

    spacing

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -13,7 +13,7 @@ const IndexPage = () => (
             <div className="w-full flex items-center flex-col">
                 <Navbar />
                 <div className="w-4/5 h-full flex flex-col items-center mt-10">
-                    <div className="w-2/3">
+                    <div className="w-4/5">
                         <img src={Photo} alt="Kimberly Kemler" className="m-auto"></img>
                         <div className="mt-4">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet quam efficitur, lacinia mauris ac, auctor ipsum. 
