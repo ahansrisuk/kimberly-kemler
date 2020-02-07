c1b7b5b4129a1566f78aaf8f1aadfbedb0492fc5
ahansrisuk <ahansrisuk@gmail.com>
2020-02-07 11:34:42 -0500

    created circle on contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -15,36 +15,40 @@ const IndexPage = () => (
             <SEO title="About" />
             <div className="w-full flex items-center flex-col">
                 <Navbar />
-                <div className="flex flex-col m-auto">
-                    <div className="flex justify-end hover:text-gray-500 pb-2 text-sm md:text-base">
-                        <a 
-                            className="mr-6"
-                            href="https://twitter.com/kimberlykemler"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            @KimberlyKemler
-                        </a>
-                        <img src={twitter} alt="twitter"></img>
-                    </div>
-                    {/* single row */}
-                    <div className="flex justify-end hover:text-gray-500 pb-2 text-sm md:text-base">
-                        <a
-                            className="mr-6"
-                            href="https://www.instagram.com/kmykem/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            @kmykem
-                        </a>
-                        <img src={instagram} alt="instagram"></img>
-                    </div>
-                    {/* single row */}
-                    <div className="flex justify-end hover:text-gray-500 text-sm md:text-base">
-                        <a className="mr-6" href="mailto:kkemler115@gmail.com">
-                            kkemler115@gmail.com
-                        </a>
-                        <img src={mail} alt="mail"></img>
+                <div class="bg-main w-full h-full flex">
+                    <div className="m-auto bg-white circle h-64 w-64 flex">
+                        <div class="m-auto">
+                            <div className="flex justify-end hover:text-gray-500 pb-2 text-sm md:text-base">
+                                <a 
+                                    className="mr-6"
+                                    href="https://twitter.com/kimberlykemler"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    @KimberlyKemler
+                                </a>
+                                <img src={twitter} alt="twitter"></img>
+                            </div>
+                            {/* single row */}
+                            <div className="flex justify-end hover:text-gray-500 pb-2 text-sm md:text-base">
+                                <a
+                                    className="mr-6"
+                                    href="https://www.instagram.com/kmykem/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    @kmykem
+                                </a>
+                                <img src={instagram} alt="instagram"></img>
+                            </div>
+                            {/* single row */}
+                            <div className="flex justify-end hover:text-gray-500 text-sm md:text-base">
+                                <a className="mr-6" href="mailto:kkemler115@gmail.com">
+                                    kkemler115@gmail.com
+                                </a>
+                                <img src={mail} alt="mail"></img>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
